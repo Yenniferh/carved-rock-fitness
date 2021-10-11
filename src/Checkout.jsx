bc40549daaf3f14d17f3cd5cd2025c1c1e4385b1
Yennifer Herrera <yenniferh@uninorte.edu.co>
2021-10-10 23:08:42 -0500

    ADD: Implement Checkout submit

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { saveShippingAddress } from './services/shippingService';
+const STATUS = {
+  IDLE: 'IDLE',
+  SUBMITTING: 'SUBMITTING',
+  SUBMITTED: 'SUBMITTED',
+  COMPLETED: 'COMPLETED',
+};
 
 // Declaring outside component to avoid recreation on each render
 const emptyAddress = {
-  city: "",
-  country: "",
+  city: '',
+  country: '',
 };
 
-export default function Checkout({ cart }) {
+export default function Checkout({ cart, emptyCart }) {
   const [address, setAddress] = useState(emptyAddress);
+  const [status, setStatus] = useState(STATUS.IDLE);
+  const [saveError, setSaveError] = useState(null);
 
   function handleChange(e) {
-    // TODO
+    e.persist(); /* Not need it in React 17+ */
+    setAddress((curAddress) => {
+      return { ...curAddress, [e.target.id]: e.target.value };
+    });
   }
 
   function handleBlur(event) {
@@ -18,7 +30,20 @@ export default function Checkout({ cart }) {
   }
 
   async function handleSubmit(event) {
-    // TODO
+    event.preventDefault();
+    setStatus(STATUS.SUBMITTING);
+    try {
+      await saveShippingAddress(address);
+      emptyCart();
+      setStatus(STATUS.COMPLETED);
+    } catch (e) {
+      setSaveError(e);
+    }
+  }
+
+  if (saveError) throw saveError;
+  if (status === STATUS.COMPLETED) {
+    return <h1>Thanks for shopping!</h1>;
   }
 
   return (
@@ -26,11 +51,11 @@ export default function Checkout({ cart }) {
       <h1>Shipping Info</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="city">City</label>
+          <label htmlFor='city'>City</label>
           <br />
           <input
-            id="city"
-            type="text"
+            id='city'
+            type='text'
             value={address.city}
             onBlur={handleBlur}
             onChange={handleChange}
@@ -38,27 +63,28 @@ export default function Checkout({ cart }) {
         </div>
 
         <div>
-          <label htmlFor="country">Country</label>
+          <label htmlFor='country'>Country</label>
           <br />
           <select
-            id="country"
+            id='country'
             value={address.country}
             onBlur={handleBlur}
             onChange={handleChange}
           >
-            <option value="">Select Country</option>
-            <option value="China">China</option>
-            <option value="India">India</option>
-            <option value="United Kingdom">United Kingdom</option>
-            <option value="USA">USA</option>
+            <option value=''>Select Country</option>
+            <option value='China'>China</option>
+            <option value='India'>India</option>
+            <option value='United Kingdom'>United Kingdom</option>
+            <option value='USA'>USA</option>
           </select>
         </div>
 
         <div>
           <input
-            type="submit"
-            className="btn btn-primary"
-            value="Save Shipping Info"
+            type='submit'
+            className='btn btn-primary'
+            value='Save Shipping Info'
+            disabled={status === STATUS.SUBMITTING}
           />
         </div>
       </form>
