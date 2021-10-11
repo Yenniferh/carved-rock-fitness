0933812134470c2a1fbcaed9c3121ed26f4df0ce
Yennifer Herrera <yenniferh@uninorte.edu.co>
2021-10-10 23:49:13 -0500

    ADD: Checkout form validation

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { saveShippingAddress } from './services/shippingService';
+
 const STATUS = {
   IDLE: 'IDLE',
   SUBMITTING: 'SUBMITTING',
@@ -17,6 +18,10 @@ export default function Checkout({ cart, emptyCart }) {
   const [address, setAddress] = useState(emptyAddress);
   const [status, setStatus] = useState(STATUS.IDLE);
   const [saveError, setSaveError] = useState(null);
+  const [touched, setTouched] = useState({});
+
+  const errors = getErrors(address);
+  const isValid = Object.keys(errors).length === 0;
 
   function handleChange(e) {
     e.persist(); /* Not need it in React 17+ */
@@ -26,21 +31,34 @@ export default function Checkout({ cart, emptyCart }) {
   }
 
   function handleBlur(event) {
-    // TODO
+    event.persist();
+    setTouched((cur) => {
+      return { ...cur, [event.target.id]: true };
+    });
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
     setStatus(STATUS.SUBMITTING);
-    try {
-      await saveShippingAddress(address);
-      emptyCart();
-      setStatus(STATUS.COMPLETED);
-    } catch (e) {
-      setSaveError(e);
+    if (isValid) {
+      try {
+        await saveShippingAddress(address);
+        emptyCart();
+        setStatus(STATUS.COMPLETED);
+      } catch (e) {
+        setSaveError(e);
+      }
+    } else {
+      setStatus(STATUS.SUBMITTED);
     }
   }
 
+  function getErrors(address) {
+    const result = {};
+    if (!address.city) result.city = 'City is required';
+    if (!address.country) result.country = 'Country is required';
+    return result;
+  }
   if (saveError) throw saveError;
   if (status === STATUS.COMPLETED) {
     return <h1>Thanks for shopping!</h1>;
@@ -49,6 +67,16 @@ export default function Checkout({ cart, emptyCart }) {
   return (
     <>
       <h1>Shipping Info</h1>
+      {!isValid && status === STATUS.SUBMITTED && (
+        <div role='alert'>
+          <p>Please fix the following errors</p>
+          <ul>
+            {Object.keys(errors).map((key) => (
+              <li key={key}>{errors[key]}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor='city'>City</label>
@@ -60,6 +88,9 @@ export default function Checkout({ cart, emptyCart }) {
             onBlur={handleBlur}
             onChange={handleChange}
           />
+          <p role='alert'>
+            {(touched.city || status === STATUS.COMPLETED) && errors.city}
+          </p>
         </div>
 
         <div>
@@ -77,6 +108,9 @@ export default function Checkout({ cart, emptyCart }) {
             <option value='United Kingdom'>United Kingdom</option>
             <option value='USA'>USA</option>
           </select>
+          <p role='alert'>
+            {(touched.country || status === STATUS.COMPLETED) && errors.country}
+          </p>
         </div>
 
         <div>
